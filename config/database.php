9d1a6d3bbfda3c5c09cbb1606f0303a398413bce
Bas Bezemer <bbezemer@gmail.com>
2016-04-26 11:24:20 +0200

    Database connections configured by OPENSHIFT_ environment variables

--- config/database.php
+++ config/database.php
@@ -44,42 +44,41 @@ return [
     |
     */
 
-    'connections' => [
+   'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
+		'sqlite' => [
+			'driver'   => 'sqlite',
+			'database' => storage_path().'/database.sqlite3',
+			'prefix'   => '',
+		],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
-        ],
+		'mysql' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', env('OPENSHIFT_MYSQL_DB_HOST', 'localhost')),
+			'port'      => env('DB_PORT', env('OPENSHIFT_MYSQL_DB_PORT', 3306)),
+			'database'  => env('DB_DATABASE', env('OPENSHIFT_APP_NAME', 'forge')),
+			'username'  => env('DB_USERNAME', env('OPENSHIFT_MYSQL_DB_USERNAME', 'forge')),
+			'password'  => env('DB_PASSWORD', env('OPENSHIFT_MYSQL_DB_PASSWORD', '')),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
+		'pgsql' => [
+			'driver'   => 'pgsql',
+			'host'     => env('DB_HOST', env('OPENSHIFT_POSTGRESQL_DB_HOST', 'localhost')),
+			'port'     => env('DB_PORT', env('OPENSHIFT_POSTGRESQL_DB_PORT', 5432)),
+			'database' => env('DB_DATABASE', env('OPENSHIFT_APP_NAME', 'forge')),
+			'username' => env('DB_USERNAME', env('OPENSHIFT_POSTGRESQL_DB_USERNAME', 'forge')),
+			'password' => env('DB_PASSWORD', env('OPENSHIFT_POSTGRESQL_DB_PASSWORD', '')),
+			'charset'  => 'utf8',
+			'prefix'   => '',
+			'schema'   => 'public',
+		]
 
-    ],
-
+	],
+	
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
